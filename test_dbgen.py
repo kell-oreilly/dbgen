b2bffb3f3cdb7cf578b87d5cd5ee534d5edadf19
kell-oreilly <kelland.oreilly@gmail.com>
2025-07-22 10:23:22 +0100

    tt

---
+++ test_dbgen.py
@@ -0,0 +1,27 @@
+import dbgen
+import pytest
+
+#Unit Tests
+def test_data_generate():
+    test_schema = {
+    "email_address": "email",
+    "local_ip": "ip",
+    "destination_ip": "ip"
+    }
+    schema_id = "test_id"
+    test_n_docs = 4
+    result = dbgen.data_generate(test_schema, test_n_docs)
+    assert len(result) == test_n_docs
+    assert all(set(doc.keys()) == {"email_address", "local_ip", "destination_ip"} for doc in result)
+
+def test_data_generate_incorrect_n_docs():
+    test_schema = {
+    "email_address": "email",
+    "local_ip": "ip",
+    "destination_ip": "ip"
+    }
+    schema_id = "test_id"
+    test_n_docs = -4
+    result = dbgen.data_generate(test_schema, test_n_docs)
+    assert len(result) == 0
+    assert result == []
