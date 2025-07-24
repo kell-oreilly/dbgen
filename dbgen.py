b15765bc7b9d071def9bcad9714b5a5e114f6295
kell-oreilly <kelland.oreilly@gmail.com>
2025-07-24 09:24:09 +0100

    Added optional parameter options to schema gen

--- dbgen.py
+++ dbgen.py
@@ -1,4 +1,5 @@
 from faker import Faker
+import random
 import json
 from flask import Flask, request
 
@@ -24,6 +25,11 @@ def init():
 def u_instructions():
     print('When prompted')
     print('Input schema key_name and datatype for documents')
+    dtype_instructions()
+    print('Then input the number of documents to generate\n')
+
+
+def dtype_instructions():
     print('Available datatypes:')
     print('-- Identification [id]')  # Format
     print('-- Name [f_name]')  # Name origin
@@ -32,50 +38,86 @@ def u_instructions():
     print('-- email Address [email]')
     # print('-- Home Address [address]')
     print('Use exact spelling (case sensitive).')
-    print('Then input the number of documents to generate\n')
 
 
-# TODO Add arguments, nested dictionary where lambda funcs are, have terminal
+# Person (name,email), location (ip, country code, currency)
 valid_dtypes = {
-    'id': None,
-    'name': lambda: faker.name(),  # TODO Add options for f/l name
-    'dob': lambda: faker.date(),  # birthdate({mode: 'age', min: 18, max: 65}),
-    'ip': lambda: faker.ipv4(),
-    'email': lambda: faker.email()  # TODO Make email name consistent across
-                                    # name and email
+    'id': lambda i, opts={}: i + 1,
+    'f_name': lambda i, opts={}: faker.first_name(),
+    'l_name': lambda i, opts={}: faker.last_name(),
+    'date': lambda i, opts={}: faker.date_between(
+        start_date=opts.get('start', '-30y'),
+        end_date=opts.get('end', 'today')
+    ).isoformat(),
+    'ip': lambda i, opts={}: faker.ipv4(),
+    'email': lambda i, opts={}: f"{opts.get('first', faker.first_name())}.{opts.get('last', faker.last_name())}@{faker.free_email_domain()}".lower(),
+    'country_code': lambda i, opts={}: faker.country_code(representation="alpha-2"),
+    #'price': lambda i, opts={}: f"{opts.get('symbol', '£')}{random.uniform(opts.get('min', 0), opts.get('max', 1000)):.{opts.get('dec', 2)}f}",
+    'para': lambda i, opts={}: faker.paragraph(),
+    'bool': lambda i, opts={}: bool(random.getrandbits(1)),
+    #'int': lambda i, opts={}:,
+    #'string': lambda i, opts={}:,
+    #'set': lambda i, opts={}: []
 }
+
+
+# convert options input into dictionary format
+def convert_opts(opt_str):
+    try:
+        return dict(
+            (k.strip(), int(v) if v.strip().isdigit() else v.strip())
+            for k, v in (pair.split("=") for pair in opt_str.split(","))
+        )
+    except Exception:
+        print("Please use valid options format: key=value, key2=value2...")
+        return None
 
 
 # Taking input, generating schema bp
 def usr_schema_build():
-    usr_schema = {}
-
-    # Schema Blueprint - Add u_instructions help
     while True:
-        key = input('Enter key name or type "finished": ')
-        while key in usr_schema.keys():
-            print('Duplicate key name found\nEnter an original key name')
-            key = input('Enter key name or type "finished": ')
-            if key == 'finished':
-                break
+        schema_name = input('Enter schema name: ')
+        confirm_schema_name = input(f'Confirm schema name?[y/n] {schema_name}: ')
+        if confirm_schema_name.lower() == 'y':
+            break
+
+    usr_schema = {"schema_name": schema_name, "fields": {}}
+
+    while True:
+        key = input('Enter key name or type "finished": ').strip()
         if key == 'finished':
             break
+        if key in usr_schema['fields']:
+            print('Duplicate key name found\nEnter original key name')
+            continue
 
         while True:
-            dtype = input(f'Enter {key} datatype or type "rename": ')
+            dtype = input(f'Enter {key} datatype or type "rename": ').strip()
             if dtype == 'rename':
-                break
-            if dtype not in valid_dtypes.keys():
-                print('\nPlease enter a valid datatype')
+                break  # go back to key input
+            if dtype not in valid_dtypes:
+                print('Please enter a valid datatype option')
                 continue
-            usr_schema[key] = dtype
-            break
-        # TODO SELECTING DTYPE OPTIONS HERE, ADD to inschema as tuple,
-        # Print user options for dtype etc
-        # Consider if multiple while loops best method
-        # Add ability to redo datatype if mistake
-        # Possibly just confirm keyname with datatype and options before
-        # moving to next key
+
+            while True:
+                opts_input = input(f'Enter {key} options or type "none" or "reselect": ').strip()
+
+                if opts_input.lower() == 'none':
+                    opts = {}
+                    break
+                elif opts_input.lower() == 'reselect':
+                    break  # reselect dtype
+                else:
+                    opts = convert_opts(opts_input)
+                    if opts is not None:
+                        break
+
+            if opts_input.lower() == 'reselect':
+                continue
+
+            usr_schema['fields'][key] = {dtype: opts}
+            break 
+
     return usr_schema
 
 
@@ -99,11 +141,23 @@ def data_generate(schema, n_docs):
 
     for i in range(n_docs):
         doc = {}
-        for key, dtype in schema.items():
-            if dtype == 'id':
-                doc[key] = i + 1
+        temp_email = {}
+
+        for field_key, field_val in schema["fields"].items():
+            dtype, opts_dic = next(iter(field_val.items()))
+
+            if dtype in ['f_name', 'l_name']:
+                generated = valid_dtypes[dtype](i, opts_dic)
+                temp_email[dtype] = generated
+                doc[field_key] = generated
+            elif dtype == 'email':
+                email_opts = {
+                    "first": temp_email.get('f_name', faker.first_name()),
+                    "last": temp_email.get('l_name', faker.last_name())
+                }
+                doc[field_key] = valid_dtypes[dtype](i, email_opts)
             else:
-                doc[key] = valid_dtypes[dtype]()
+                doc[field_key] = valid_dtypes[dtype](i, opts_dic)
         gen_data.append(doc)
 
     return gen_data
@@ -118,8 +172,8 @@ def main():
     num_docs = usr_num_docs()
     generated_data = data_generate(schema, num_docs)
 
-    # json_schema = json.dumps(schema, indent=2)
-    # print(f'Schema:\n{json_schema}')
+    json_schema = json.dumps(schema, indent=2)
+    print(f'Schema:\n{json_schema}')
     print('\nData:')
     for doc in generated_data:
         print(json.dumps(doc))
